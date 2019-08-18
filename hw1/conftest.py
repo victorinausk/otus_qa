df763c064c45271a83ff6a47869074197475e651
victorinausk <victoria.uskova@gmail.com>
2019-08-18 21:52:46 +0300

    first hw fix

--- hw1/conftest.py
+++ hw1/conftest.py
@@ -1,20 +1,23 @@
 import pytest
 
+
 @pytest.fixture(scope="session")
 def session_fixture():
     print("\nСессия fixture")
     yield
     print("\nЗакрытие сессии fixture")
 
+
 @pytest.fixture(scope="module")
 def module_fixture(request):
     print("\nМодуль fixture")
+
     def fin():
         print("\nЗакрытие модуля fixture")
 
     request.addfinalizer(fin)
 
+
 @pytest.fixture()
 def function_fixture():
     print("\nФункция fixture")
-
--- hw1/test_one.py
+++ hw1/test_one.py
@@ -1,92 +1,75 @@
 # -*- coding: UTF-8 -*-
 
-import pytest
-
-
-"""Проверяем, что 8 элемент списка = 8"""
 
 def test_item_of_list(session_fixture, module_fixture, function_fixture):
-    list = [1, 2, 3, 4, 5, 6, 7, 8]
-
+    """Проверяем, что 8 элемент списка = 8"""
+    mylist = [1, 2, 3, 4, 5, 6, 7, 8]
     print("\ntest N1: ")
-    assert list[7] == 8, "test N1 is not passed"
+    assert mylist[7] == 8, "test N1 is not passed"
 
 
-"""Проверяем, что оператор + складывает 2 строки"""
-
-def test_summ_of_two_strings(session_fixture, module_fixture, function_fixture):
+def test_sum_of_two_strings(session_fixture, module_fixture, function_fixture):
+    """Проверяем, что оператор + складывает 2 строки"""
     str1 = 'well'
     str2 = 'done'
-    str = str1+str2
+    answ = str1 + str2
     print("\ntest N2: ")
-    assert str == 'welldone', "test N2 is not passed"
+    assert answ == 'welldone', "test N2 is not passed"
 
-
-"""Проверяем. что оператор * умножает строку на заданное число"""
 
 def test_multiply_number_to_string(session_fixture, module_fixture, function_fixture):
-    str = '123'
-    multiplied_str = 2*str
+    """Проверяем. что оператор * умножает строку на заданное число"""
+    str1 = '123'
+    multiplied_str = 2 * str1
     print("\ntest N3 ")
     assert multiplied_str == '123123', "test N3 is not passed"
 
 
-
-"""Проверяем, что функция clear отчищает список"""
-
 def test_clear_of_list(session_fixture, module_fixture, function_fixture):
-    list = ['aa', 'bb', 'cc']
-    list.clear()
+    """Проверяем, что функция clear отчищает список"""
+    mylist = ['aa', 'bb', 'cc']
+    mylist.clear()
     print("\ntest N4")
-    assert len(list) == 0, "test N4 is not passed"
+    assert len(mylist) == 0, "test N4 is not passed"
 
 
-
-
-"""Проверяем, что функция count находит число вхождений в список"""
-
-def test_ammount_of_x_in_list(session_fixture, module_fixture, function_fixture):
-    list = ['qq', 'x', 'aaz', 'x']
-    ammount = list.count('x')
+def test_amount_of_x_in_list(session_fixture, module_fixture, function_fixture):
+    """Проверяем, что функция count находит число вхождений в список"""
+    mylist = ['qq', 'x', 'aaz', 'x']
+    amount = mylist.count('x')
     print("\ntest N5")
-    assert ammount == 2, print("test N5 is not passed")
+    assert amount == 2, print("test N5 is not passed")
 
-
-"""Проверяем, что функция reverse переворачивает список"""
 
 def test_reversed_list(session_fixture, module_fixture, function_fixture):
-    list = ['first one', 'middle one', 'last one']
-    list.reverse()
+    """Проверяем, что функция reverse переворачивает список"""
+    mylist = ['first one', 'middle one', 'last one']
+    mylist.reverse()
     first_of_reversed = list[0]
     print("\ntest N6")
     assert first_of_reversed == 'last one', print("not passed")
 
 
-
-"""Проверяем. что множество содержит только уникальные элементы"""
-
 def test_set_consist_of_unique_elements(session_fixture, module_fixture, function_fixture):
+    """Проверяем. что множество содержит только уникальные элементы"""
     s = set()
     s.add(1)
     s.add(2)
     s.add(1)
-    lenth = len(s)
+    length = len(s)
     print("\ntest N7")
+    assert length == 2, print("not passed")
 
-    assert lenth == 2 , print("not passed")
-
-
-"""Проверяем, что по ключу можно получить значение из словаря"""
 
 def test_getting_dictionary_value_by_key(session_fixture, module_fixture, function_fixture):
+    """Проверяем, что по ключу можно получить значение из словаря"""
     d = dict(key1='value1', key2='value2', key3='value3')
     print("test N8")
     assert d['key1'] == 'value1', print("not passed")
 
 
-"""Проверяем, что значения переменных поменены"""
-
 def test_switch_values(session_fixture, module_fixture, function_fixture):
+    """Проверяем, что значения переменных поменены"""
     a = 5
     b = 10
     a, b = b, a
@@ -94,23 +77,19 @@ def test_switch_values(session_fixture, module_fixture, function_fixture):
     assert a == 10, print("not passed")
 
 
-
-
-"""Проверяем, что функция insert вставляет значение в список"""
-
 def test_insert_value(session_fixture, module_fixture, function_fixture):
-    l = ['first one', 'middle one', 'last one']
-    l.insert(0, 'new first one')
+    """Проверяем, что функция insert вставляет значение в список"""
+    mylist = ['first one', 'middle one', 'last one']
+    mylist.insert(0, 'new first one')
     print("test N10")
-    assert l[0] == 'new first one', print("not passed")
+    assert mylist[0] == 'new first one', print("not passed")
 
 
 def inc(x):
     return x + 1
-"""Проверяем, что функция инкримента увеличивает на 1 """
+
+
 def test_answer():
+    """Проверяем, что функция инкримента увеличивает на 1 """
     print("test N11")
-    assert inc(3) == 4, print ("not passed")
-
-
-
+    assert inc(3) == 4, print("not passed")
