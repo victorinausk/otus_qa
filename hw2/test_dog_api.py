ad192491266dda01297b0a710fa3968bc2389ceb
Victoria <victoria.uskova@google.com>
2019-09-07 11:13:07 +0300

    hw2

--- hw2/test_dog_api.py
+++ hw2/test_dog_api.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import pytest
+import requests
 
 ENDPOINTS = [
     '/breeds/list/all',
@@ -15,6 +16,7 @@ ENDPOINTS = [
     '/breed/collie/border/images/random/10',
 ]
 
+
 @pytest.mark.parametrize('endpoint', ENDPOINTS)
 def test_dogapi(dogceo, endpoint):
     """ Test GET requests for https://dog.ceo/api """
@@ -23,3 +25,25 @@ def test_dogapi(dogceo, endpoint):
     assert response.status_code == 200
     assert response.reason == 'OK'
     assert response.headers['Content-Type'] == 'application/json'
+
+
+@pytest.mark.parametrize("test_input", ["terrier", "pointer", "hound", "spaniel"])
+class TestWithBreed:
+    # pylint: disable=too-few-public-methods
+    """ Test GET requests for https://dog.ceo/api """
+
+    @classmethod
+    def test_img_by_breed(cls, test_input):
+        """
+        Проверка соответствия выборки изображений согласно породе ..
+        :param test_input:
+        """
+        url = "https://dog.ceo/api/breed/" + test_input + "/images"
+        r = requests.get(url)
+        img = r.json()['message']
+        errors = 0
+        for i in img:
+            if test_input not in i:
+                print(test_input + " == " + i)
+                errors += 1
+        assert errors == 0
