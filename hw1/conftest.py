551f6dc2dd2c8a1a3c9f38560b90a8859b38bb71
Victoria <victoria.uskova@google.com>
2019-09-08 09:36:30 +0300

    hw2

--- hw1/conftest.py
+++ hw1/conftest.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
+"""Тестирование базовых  Фикстур"""
 
 import pytest
 
 
 @pytest.fixture(scope="session")
 def session_fixture():
+    """Тестирование базовых  Фикстур ссесия"""
     print("\nСессия fixture")
     yield
     print("\nЗакрытие сессии fixture")
@@ -12,9 +14,11 @@ def session_fixture():
 
 @pytest.fixture(scope="module")
 def module_fixture(request):
+    """Тестирование базовых  Фикстур модуль"""
     print("\nМодуль fixture")
 
     def fin():
+        """Тестирование базовых  Фикстур модуль"""
         print("\nЗакрытие модуля fixture")
 
     request.addfinalizer(fin)
@@ -22,4 +26,5 @@ def module_fixture(request):
 
 @pytest.fixture()
 def function_fixture():
+    """Тестирование базовых  Фикстур"""
     print("\nФункция fixture")
