0f983062dd0b0c208b44fb50c47e7f77b7b39922
Victoria <victoria.uskova@google.com>
2019-09-10 22:49:43 +0300

    travis update, code format

--- hw4/conftest.py
+++ hw4/conftest.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 """Базовая настройка сервисов"""
+import time
 import pytest
 import testcontainers.compose
-import time
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions, FirefoxOptions
 
@@ -15,7 +15,7 @@ def module_fixture(request):
     print("\nЗапуск docker-compose")
     compose = testcontainers.compose.DockerCompose(COMPOSE_PATH)
     compose.start()
-    time.sleep(100)
+    time.sleep(10)
 
     def fin():
         """Остановка окружения"""
@@ -28,25 +28,28 @@ def module_fixture(request):
 
 @pytest.fixture
 def chrome_browser():
+    """chrome browser"""
     options = ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--start-fullscreen')
-    wd = webdriver.Chrome(options=options)
-    yield wd
-    wd.quit()
+    web_driver = webdriver.Chrome(options=options)
+    yield web_driver
+    web_driver.quit()
 
 
 @pytest.fixture
 def firefox_browser():
+    """firefox browser"""
     options = FirefoxOptions()
     options.add_argument('--headless')
     options.add_argument('--start-fullscreen')
-    wd = webdriver.Firefox(options=options)
-    yield wd
-    wd.quit()
+    web_driver = webdriver.Firefox(options=options)
+    yield web_driver
+    web_driver.quit()
 
 
 def pytest_collection_modifyitems(items, config):
+    """modifyitens"""
     browser = config.getoption('browser')
     if browser is not None:
         selected = []
@@ -65,5 +68,6 @@ def pytest_collection_modifyitems(items, config):
 
 
 def pytest_addoption(parser):
+    """options"""
     parser.addoption('--browser', help='Run tests only for certain browser.')
     parser.addoption('--opencart_url', default='http://127.0.0.1/')
--- hw4/test_opencart.py
+++ hw4/test_opencart.py
@@ -1,9 +1,11 @@
 """ Tests for opencart main page """
 
+
 def test_chrome(chrome_browser, request, module_fixture):
     """ Test open opencart main page in Chrome """
     chrome_browser.get(request.config.getoption('--opencart_url'))
     assert chrome_browser.current_url == request.config.getoption('--opencart_url')
+
 
 def test_firefox(firefox_browser, request, module_fixture):
     """ Test open opencart main page in Firefox """
