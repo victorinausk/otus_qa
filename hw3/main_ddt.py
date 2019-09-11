02425cbe59f9f2d2814caea236aa1c2a5114bde9
Victoria <victoria.uskova@google.com>
2019-09-11 07:55:25 +0300

    travis update, code format

--- hw3/main_ddt.py
+++ hw3/main_ddt.py
@@ -6,7 +6,7 @@
 def gen_file():
     """Подготовка тестовых данных"""
 
-    with open('data.csv', mode='r+', encoding='windows-1251') as my_file:
+    with open('hw3/data.csv', mode='r+', encoding='windows-1251') as my_file:
         f = my_file.read().splitlines()
         # убираем первую строку с информацией о данных таблицы
         f.pop(0)
@@ -39,6 +39,6 @@ def gen_file():
         final = ((name, city, credit_card, deposit, mortgage) for name in names for city in cities
                  for credit_card in credit_cards for deposit in deposits for mortgage in mortgages)
 
-        with open('result_data.txt', 'a') as f_txt:
+        with open('hw3/result_data.txt', 'a') as f_txt:
             for i in final:
                 f_txt.write(' '.join(i) + '\n')
