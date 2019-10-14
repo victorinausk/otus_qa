6673431a89045338fc8a1fdbc455e7bac60435a7
Victoria <victoria.uskova@google.com>
2019-10-14 17:55:47 +0300

    Upload test created

--- hw12/install_browsermob.sh
+++ hw12/install_browsermob.sh
@@ -4,5 +4,5 @@ rm -r ./browsermob/
 mkdir ./browsermob                         && \
 cd ./browsermob                            && \
 curl -LJO https://github.com/lightbody/browsermob-proxy/releases/download/browsermob-proxy-2.1.4/browsermob-proxy-2.1.4-bin.zip  && \
-unzip -d browsermob-proxy-2.1.4-bin.zip                      && \
+unzip -j browsermob-proxy-2.1.4-bin.zip                      && \
 rm browsermob-proxy-2.1.4-bin.zip
