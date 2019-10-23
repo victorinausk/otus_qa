65db1720102f1bc8e106e36cb44f23fa5d4570d9
Victoria <victoria.uskova@google.com>
2019-10-23 10:39:11 +0300

    Remote test created

--- hw13/test_remote_server.py
+++ hw13/test_remote_server.py
@@ -50,7 +50,7 @@ def test_grid(browser):
     """Тестирование грида в докере"""
 
     browser.get("http://www.google.com")
-    if not "Google" in browser.title:
+    if "Google" not in browser.title:
         raise Exception("Unable to load google page!")
     elem = browser.find_element_by_name("q")
     elem.click()
