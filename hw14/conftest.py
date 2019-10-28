fe34e6363b9fbac7c6f2e5024d5f0c4fcada650b
victory <victoria.uskova@gmail.com>
2019-10-28 20:29:33 +0300

    Remote test created docker-compose logic fixed in travis

--- hw14/conftest.py
+++ hw14/conftest.py
@@ -22,15 +22,15 @@ def module_fixture(request):
 
 @pytest.mark.usefixtures("environment_info")
 @pytest.fixture(scope='session', autouse=True)
-def configure_html_report_env(request, module_info):
+def configure_html_report_env(request, get_module_info):
     request.config._metadata.update(
-        {"Env": os.environ, "Modules": module_info}
+        {"Env": os.environ, "Modules": get_module_info}
     )
     yield
 
 
 @pytest.fixture(scope='session')
-def module_info():
+def get_module_info():
     lst = []
     for pkg in pkgutil.iter_modules():
         lst.append(pkg.name)
