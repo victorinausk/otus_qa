be4b6919e96f4dd431ac7f2b7cd69dcbec36cf53
victory <victoria.uskova@gmail.com>
2019-11-11 14:06:11 +0300

    HW24

--- hw24/docker_build.sh
+++ hw24/docker_build.sh
@@ -1,4 +1,4 @@
 echo "building docker"
 LATEST_CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")
 echo "Latest chrome driver version:" $LATEST_CHROMEDRIVER_VERSION
-docker build .
+docker build --build-arg CROMEDRIVER_RELEASE=$LATEST_CHROMEDRIVER_VERSION .
