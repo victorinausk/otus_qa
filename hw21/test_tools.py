bd26038e0a9a3b4e79dff580413c89486c116729
victory <victoria.uskova@gmail.com>
2019-11-12 17:49:10 +0300

    HW24

---
+++ hw21/test_tools.py
@@ -0,0 +1,77 @@
+# -*- coding: UTF-8 -*-
+"""Пример тестов ssh, sftp"""
+import os
+
+import paramiko
+
+
+def find_file(file_name):
+    """ Find file in sub dirs """
+
+    rootdir = os.getcwd()
+
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            filepath = subdir + os.sep + file
+
+            if filepath.endswith(file_name):
+                return filepath
+    raise Exception("Files not found" + file_name + "\n")
+
+
+class ssh(object):
+    """docstring"""
+
+    def __init__(self, host='localhost', user='master', pwd='master', port=2222, internal_port=22):
+        """Constructor"""
+        self.host = host
+        self.user = user
+        self.pwd = pwd
+        self.port = port
+        self.internal_port = internal_port
+        self.client = None
+
+    def create_client(self):
+        """
+        Connect by SSH
+        """
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname=self.host, username=self.user, password=self.pwd, port=self.port)
+        self.client = client
+        return self.client
+
+    def execute_command(self, cmd):
+        """
+        Execute ssh or sftp command
+        """
+        stdin, stdout, stderr = self.client.exec_command(cmd)
+        return stdout.read() + stderr.read()
+
+    def __del__(self):
+        self.client.close()
+
+
+def test_ssh():
+    """
+     Test ssh
+    """
+    ssh_client = ssh()
+    ssh_client.create_client()
+    ssh_client.execute_command('rm -rf *')
+    data = ssh_client.execute_command('echo "hello world">> test.txt')
+    print(data)
+    data = ssh_client.execute_command('ls')
+    assert data == b'test.txt\n'
+
+
+def test_sftp_upload():
+    """
+     Test sftp upload
+    """
+    ssh_client = ssh()
+    ssh_client.create_client()
+    ftp = ssh_client.create_client().open_sftp()
+    data = ftp.put(find_file('README.md'), 'README.md')
+    data = ssh_client.execute_command('ls')
+    assert data == b'README.md\ntest.txt\n'
