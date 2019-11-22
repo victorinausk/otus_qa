cab802fcb7a6d4f96c0dcded2b1917132f7e1028
victory <victoria.uskova@gmail.com>
2019-11-22 16:34:28 +0300

    HW27

--- hw27/test_package.py
+++ hw27/test_package.py
@@ -2,6 +2,10 @@ import pkg_resources
 
 
 def test_is_wheel_exists():
+    """
+    Проверка установленного пакета
+    :return:
+    """
     installed_packages = pkg_resources.working_set
     installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
                                       for i in installed_packages])
