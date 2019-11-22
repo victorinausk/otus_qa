a80e8fb8c71ff5c35d2792b08dc400513864bb8a
victory <victoria.uskova@gmail.com>
2019-11-22 16:30:25 +0300

    HW27

---
+++ hw27/test_package.py
@@ -0,0 +1,8 @@
+import pkg_resources
+
+
+def test_is_wheel_exists():
+    installed_packages = pkg_resources.working_set
+    installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+                                      for i in installed_packages])
+    assert installed_packages_list.__contains__('otus-qa==1')
